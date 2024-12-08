c8f489a8f680a30eb3caacf5faf8cf2b56173179
deepaung <i.deepaung@gmail.com>
2024-12-08 14:07:54 +0700

    fix: fix migration.sql file

--- public-server/migrations/seed.sql
+++ public-server/migrations/seed.sql
@@ -2,12 +2,12 @@ BEGIN;
 
 INSERT INTO "users" ("username", "email", "password", "is_admin")
   VALUES ('DeepAung', 'i.deepaung@gmail.com', '', FALSE),
-  ('admin', 'admin@gmail.com', '', 1);
+  ('admin', 'admin@gmail.com', '', TRUE);
 
 INSERT INTO "tasks" ("display_name", "url_name", "content_url", "testcase_count")
   VALUES ('Two Sum', 'two_sum', '', 10);
 
-INSERT INTO "submissions" ("user_id", "task_id", "code", "results")
-  VALUES (1, 1, 'for _ in range(len(int(input()))): print(int(input()) + int(input()))', 'PPPPPPPPPP');
+INSERT INTO "submissions" ("user_id", "task_id", "code", "language", "results")
+  VALUES (1, 1, 'for _ in range(len(int(input()))): print(int(input()) + int(input()))', 'python', 'PPPPPPPPPP');
 
 COMMIT;
