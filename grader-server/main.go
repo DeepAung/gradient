a8804db14bb71d5a760e07724004bc4d9b2dc9fc
DeepAung <i.deepaung@gmail.com>
2025-03-09 19:09:34 +0700

    refactor: comment out the main and server file

--- grader-server/main.go
+++ grader-server/main.go
@@ -1,29 +1,29 @@
 package main
 
-import (
-	_ "embed"
-	"flag"
-)
-
-var (
-	address       = flag.String("address", "localhost:50051", "grader server's address")
-	gcpBucketName = flag.String("gcp-bucket-name", "gradient-bucket-dev", "GCP bucket name")
-	maxGoroutines = 5
-)
-
-func main() {
-	// flag.Parse()
-	// lis, err := net.Listen("tcp", *address)
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-	//
-	// storer := storer.NewGcpStorer(*gcpBucketName, maxGoroutines)
-	// graderServer := server.NewGraderServer(cfg, storer)
-	//
-	// grpcServer := grpc.NewServer()
-	// proto.RegisterGraderServer(grpcServer, graderServer)
-	//
-	// log.Printf("grader server running on address %s", *address)
-	// grpcServer.Serve(lis)
-}
+// import (
+// 	_ "embed"
+// 	"flag"
+// )
+//
+// var (
+// 	address       = flag.String("address", "localhost:50051", "grader server's address")
+// 	gcpBucketName = flag.String("gcp-bucket-name", "gradient-bucket-dev", "GCP bucket name")
+// 	maxGoroutines = 5
+// )
+//
+// func main() {
+// 	flag.Parse()
+// 	lis, err := net.Listen("tcp", *address)
+// 	if err != nil {
+// 		log.Fatalf("failed to listen: %v", err)
+// 	}
+//
+// 	storer := storer.NewGcpStorer(*gcpBucketName, maxGoroutines)
+// 	graderServer := server.NewGraderServer(cfg, storer)
+//
+// 	grpcServer := grpc.NewServer()
+// 	proto.RegisterGraderServer(grpcServer, graderServer)
+//
+// 	log.Printf("grader server running on address %s", *address)
+// 	grpcServer.Serve(lis)
+// }
--- grader-server/server/server.go
+++ grader-server/server/server.go
@@ -1,120 +1,135 @@
 package server
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"os"
-	"time"
-
-	"github.com/DeepAung/gradient/grader-server/graderconfig"
-	"github.com/DeepAung/gradient/grader-server/pkg/checker"
-	"github.com/DeepAung/gradient/grader-server/pkg/runner"
-	"github.com/DeepAung/gradient/grader-server/proto"
-	"github.com/DeepAung/gradient/website-server/pkg/storer"
-	"github.com/google/uuid"
-	"google.golang.org/grpc"
-)
-
-type graderServer struct {
-	proto.UnimplementedGraderServer
-	cfg    *graderconfig.Config
-	storer storer.Storer
-}
-
-func NewGraderServer(cfg *graderconfig.Config, storer storer.Storer) *graderServer {
-	return &graderServer{
-		cfg:    cfg,
-		storer: storer,
-	}
-}
-
-func (s *graderServer) Grade(
-	input *proto.Input,
-	stream grpc.ServerStreamingServer[proto.Result],
-) error {
-	// Pull testcases from taskId
-	remoteDir := fmt.Sprintf("testcases/%d", input.TaskId)
-	testcasesDir := fmt.Sprintf("tmp/testcases/%d", input.TaskId)
-	cnt, err := s.storer.DownloadFolder(remoteDir, testcasesDir)
-	if err != nil {
-		return err
-	}
-	testcaseCount := cnt / 2
-	// End
-
-	// Create code file/folder
-	submissionId := uuid.NewString()
-	submissionDir := fmt.Sprintf("tmp/submissions/%s", submissionId)
-	languageInfo, ok := s.cfg.GetLanguageInfoFromProto(input.Language)
-	if !ok {
-		return errors.New("invalid language")
-	}
-	codeExt := languageInfo.Extension
-
-	codeFilename := fmt.Sprintf("tmp/submissions/%s/%s", submissionId, "code"+codeExt)
-	if err := os.MkdirAll(submissionDir, os.ModePerm); err != nil {
-		return err
-	}
-	codeFile, err := os.Create(codeFilename)
-	if err != nil {
-		return err
-	}
-	codeFile.Write([]byte(input.Code))
-	codeFile.Close()
-	// End
-
-	runner := runner.NewCodeRunner(s.cfg)
-	checker := checker.NewCodeChecker()
-
-	// Build code
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	ok, status := runner.Build(ctx, input.Language, codeFilename)
-	if !ok {
-		stream.Send(&proto.Result{Status: status})
-		return nil
-	}
-	// End
-
-	// Run and Check code
-	for i := 1; i <= testcaseCount; i++ {
-		inputFilename := fmt.Sprintf("%s/%02d.in", testcasesDir, i)
-
-		ok, result := runner.Run(ctx, input.Language, codeFilename, inputFilename)
-		if !ok {
-			stream.Send(&result)
-			continue
-		}
-
-		outputFilename := fmt.Sprintf("%s/%02d.out", testcasesDir, i)
-		resultFilename := fmt.Sprintf("%s/%02d.result", submissionDir, i)
-		ok, err := checker.CheckFile(ctx, outputFilename, resultFilename)
-		if err != nil {
-			return err
-		}
-		if ok {
-			stream.Send(&proto.Result{
-				Status: proto.StatusType_PASS,
-				Time:   result.Time,
-				Memory: result.Memory,
-			})
-		} else {
-			stream.Send(&proto.Result{
-				Status: proto.StatusType_INCORRECT,
-				Time:   result.Time,
-				Memory: result.Memory,
-			})
-		}
-	}
-	// End
-
-	// Delete code file/folder
-	if err := os.RemoveAll(submissionDir); err != nil {
-		return err
-	}
-	// End
-
-	return nil
-}
+// import (
+// 	"context"
+// 	"fmt"
+// 	"os"
+// 	"time"
+//
+// 	"github.com/DeepAung/gradient/grader-server/graderconfig"
+// 	"github.com/DeepAung/gradient/grader-server/pkg/checker"
+// 	"github.com/DeepAung/gradient/grader-server/pkg/runner"
+// 	"github.com/DeepAung/gradient/grader-server/proto"
+// 	"github.com/DeepAung/gradient/website-server/pkg/storer"
+// 	"github.com/google/uuid"
+// 	"google.golang.org/grpc"
+// )
+//
+// type graderServer struct {
+// 	proto.UnimplementedGraderServer
+// 	runner  runner.CodeRunner
+// 	checker checker.CodeChecker
+// 	cfg     *graderconfig.Config
+// 	storer  storer.Storer
+// }
+//
+// func NewGraderServer(
+// 	runner runner.CodeRunner,
+// 	checker checker.CodeChecker,
+// 	cfg *graderconfig.Config,
+// 	storer storer.Storer,
+// ) *graderServer {
+// 	return &graderServer{
+// 		runner:  runner,
+// 		checker: checker,
+// 		cfg:     cfg,
+// 		storer:  storer,
+// 	}
+// }
+//
+// /*
+// 1. pull testcases from into tmp/{taskId} (01.in, 01.out)
+// 2. touch code.cpp inside tmp/{taskId}
+// 3. compile code (send stream)
+// 4. execute code on each testcases (send stream)
+// 5. cleanup
+// */
+// func (s *graderServer) Grade(
+// 	input *proto.Input,
+// 	stream grpc.ServerStreamingServer[proto.Result],
+// ) error {
+// 	// Pull testcases from taskId
+// 	remoteDir := fmt.Sprintf("testcases/%d", input.TaskId)
+// 	testcasesDir := fmt.Sprintf("tmp/testcases/%d", input.TaskId)
+// 	cnt, err := s.storer.DownloadFolder(remoteDir, testcasesDir)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	testcaseCount := cnt / 2
+// 	// End
+//
+// 	// Create new code file and write content
+// 	submissionId := uuid.NewString()
+// 	submissionDir := fmt.Sprintf("tmp/submissions/%s", submissionId)
+// 	languageInfo, ok := s.cfg.GetLanguageInfoFromProto(input.Language)
+// 	if !ok {
+// 		return runner.ErrInvalidLanguage
+// 	}
+// 	codeExt := languageInfo.Extension
+//
+// 	codeFilename := fmt.Sprintf("tmp/submissions/%s/%s", submissionId, "code"+codeExt)
+// 	if err := os.MkdirAll(submissionDir, os.ModePerm); err != nil {
+// 		return err
+// 	}
+// 	codeFile, err := os.Create(codeFilename)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	codeFile.Write([]byte(input.Code))
+// 	codeFile.Close()
+// 	// End
+//
+// 	// Build code
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+//
+// 	ok, status := s.runner.Build(ctx, input.Language, codeFilename)
+// 	if !ok {
+// 		stream.Send(&proto.Result{Status: status})
+// 		return nil
+// 	}
+// 	// End
+//
+// 	// Run and Check code
+// 	for i := 1; i <= testcaseCount; i++ {
+// 		inputFilename := fmt.Sprintf("%s/%02d.in", testcasesDir, i)
+//
+// 		ok, result, err := s.runner.Run(ctx, input.Language, codeFilename, inputFilename)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if !ok {
+// 			stream.Send(&result)
+// 			continue
+// 		}
+//
+// 		outputFilename := fmt.Sprintf("%s/%02d.out", testcasesDir, i)
+// 		resultFilename := fmt.Sprintf("%s/%02d.result", submissionDir, i)
+// 		ok, err = s.checker.CheckFile(ctx, outputFilename, resultFilename)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if ok {
+// 			stream.Send(&proto.Result{
+// 				Status: proto.StatusType_PASS,
+// 				Time:   result.Time,
+// 				Memory: result.Memory,
+// 			})
+// 		} else {
+// 			stream.Send(&proto.Result{
+// 				Status: proto.StatusType_INCORRECT,
+// 				Time:   result.Time,
+// 				Memory: result.Memory,
+// 			})
+// 		}
+// 	}
+// 	// End
+//
+// 	// Delete code file/folder
+// 	if err := os.RemoveAll(submissionDir); err != nil {
+// 		return err
+// 	}
+// 	// End
+//
+// 	return nil
+// }
